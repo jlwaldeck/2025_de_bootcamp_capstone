44756578a8e6588b28a18f5afd23b586c3e0aa31
jlwaldeck <jlwaldeck>
2025-02-28 04:20:20 +0000

    Commit everything from Databricks
    This is the current working version plus extra supporting files.

---
+++ DLT Load Historical Data.sql
@@ -0,0 +1,13 @@
+-- Databricks notebook source
+CREATE OR REFRESH STREAMING LIVE TABLE jw_historical_hoops_bronze
+COMMENT "Ingest ncaa player_game csv data"
+TBLPROPERTIES ("quality" = "bronze")
+AS SELECT 
+ *
+FROM (
+  SELECT * FROM cloud_files(
+    "/Volumes/tabular/dataexpert/jw_capstone/clean_csv/", 
+    "csv", 
+    map("header", "true", "inferSchema", "true")
+  )
+)
